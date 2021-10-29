8a1f6bacde054d727eab21037e2cc2b89bf1cdfa
Alejandro Reyna <AleejandroReyna@gmail.com>
2021-10-28 22:54:28 -0600

    RC-01: Added ui kit to packages (#2)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,13 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import UIkit from 'uikit'
+import Icons from 'uikit/dist/js/uikit-icons'
+import "./application.sass"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+// loads the Icon plugin
+UIkit.use(Icons);
