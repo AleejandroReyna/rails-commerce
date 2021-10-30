af7cc054a4db811b2dbf8edc024bef04bb496f4b
Alejandro Reyna <aleejandroreyna@gmail.com>
2021-10-29 23:31:35 -0600

    issue-3: Added users controller
    Added controller, view, methods without functionality and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sign_up', to: "users#new"
+  post 'sign_up', to: "users#create"
   get '/', to: "pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
