d0e685df5e830edbd6f57cb61d902139ba3c24ac
Alejandro Reyna <aleejandroreyna@gmail.com>
2021-10-31 23:03:06 -0600

    issue-3: Added create action for user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,16 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(create_user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: 'Successfully created account'
+    else
+      render :new, alert: "Check your data and try again"
+    end
+  end
+
+  def create_user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
