0a1740628b622d60d49b2f5e599467b72431c698
Alejandro Reyna <aleejandroreyna@gmail.com>
2021-10-31 22:25:51 -0600

    issue-3: Change route for post create user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'sign_up', to: "users#new"
-  post 'sign_up', to: "users#create"
-  get '/', to: "pages#home"
+  post 'create_user', to: "users#create"
+  root "pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
