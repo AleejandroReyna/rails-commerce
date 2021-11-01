1dbfab2e190f4834a68cafe57110dd0234a235c7
Alejandro Reyna <aleejandroreyna@gmail.com>
2021-10-31 23:02:44 -0600

    issue-3: Added validations for fields on sign up view

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 end
